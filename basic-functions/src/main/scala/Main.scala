657563f6a70a9e6355e072850a6dae1220e3b22b
jahantech <jahansyed@hotmail.co.uk>
2018-02-25 16:35:59 +0000

    File read function

--- basic-functions/src/main/scala/Main.scala
+++ basic-functions/src/main/scala/Main.scala
@@ -1,10 +1,20 @@
+import scala.io.Source
+
 object Main extends App {
   println("Scala Program")
   println(BasicSum(1,2))
-
+  FileRead("/home/jahans/repos/scala/basic-functions/resources/test-read-file.txt")
 
   def BasicSum (a:Int, b:Int): Int ={
     return a+b
+  }
+
+  def FileRead(fileName:String):Unit ={
+
+    for (line <- Source.fromFile(fileName).getLines){
+      println(line)
+    }
+
   }
 }
 

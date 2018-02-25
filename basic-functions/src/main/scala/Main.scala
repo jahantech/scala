7a96e35a056460a8f126df62afa03c0e53c8fc6a
jahantech <jahansyed@hotmail.co.uk>
2018-02-25 16:42:44 +0000

    Functions from different object

--- basic-functions/src/main/scala/Main.scala
+++ basic-functions/src/main/scala/Main.scala
@@ -4,7 +4,12 @@ object Main extends App {
   println("Scala Program")
   println(BasicSum(1,2))
   FileRead("/home/jahans/repos/scala/basic-functions/resources/test-read-file.txt")
+  MultiplicationFactor.ByTwo(10)
 
+  MultiplicationFactor.ByTen(100)
+
+  MultiplicationFactor.JustLocal()
+  
   def BasicSum (a:Int, b:Int): Int ={
     return a+b
   }
@@ -18,4 +23,20 @@ object Main extends App {
   }
 }
 
+
+object MultiplicationFactor {
+
+  var local_x:Int = 2
+  def ByTwo(x:Int):Unit ={
+    println(x*2)
+  }
+
+  def ByTen(x:Int):Unit ={
+    println(x*10)
+  }
+
+  def JustLocal():Unit ={
+    println(local_x)
+  }
+}
 
